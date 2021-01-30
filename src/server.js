39cfc04151c4e696a000b6e772f0b3452f8b1808
Dan McElroy <daniel@mcelroys.net>
2021-01-30 19:00:42 +0100

    Added leaving broadcast

--- src/server.js
+++ src/server.js
@@ -25,9 +25,14 @@ app.get('', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
